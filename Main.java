3f7695cbd665d68336e6519bb7c2c5a157901566
Jaime Alberto Cuervo Valencia <121111282+jaimealbertocuervo@users.noreply.github.com>
2023-01-08 15:48:16 -0500

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+            int resultado = sumar(5, 7, 2);
+      System.out.println(resultado); 
+    }
+  
+    public static int sumar(int a, int b, int c) {
+            return a + b + c;
+    }
+  }
+   
